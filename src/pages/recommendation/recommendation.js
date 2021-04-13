90c66f3a9bb13095288252f71134288d03d528d7
gabriella-guimaraes <gabigara13@gmail.com>
2021-04-13 15:21:59 -0300

    esqueleto página criação de recomendações

--- src/pages/recommendation/recommendation.js
+++ src/pages/recommendation/recommendation.js
@@ -2,7 +2,119 @@ export const Recommendation = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
-      <h1>Card de recomendação</h1>
+  <div class="recommendation-page">
+    <h1>Escreva uma recomendação!</h1>
+    <form id="recommendation-form">
+      <label class="recommendation-page" for="userAddressee">Para:</label>
+      <input class="recommendation-page" id="userAddressee" placeholder="" required>
+
+      <button class="recommendation-page" id="inspirationBtn">Inspiração do projeto</button>
+      <button class="recommendation-pagem" id="differenceBtn">Diferença no dia-a-dia</button>
+      <button class="recommendation-page" id="referenceBtn">Referência em atitude Safra</button>
+
+    </form>
+  </div>
   `;
+
+  const userAddressee = rootElement.querySelector('#userAddressee');
+  const inspirationBtn = rootElement.querySelector('#inspirationBtn');
+  const differenceBtn = rootElement.querySelector('#differenceBtn');
+  const referenceBtn = rootElement.querySelector('#referenceBtn');
+  const recommendationForm = rootElement.querySelector('#recommendation-form');
+
+  inspirationBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('clicou');
+    const modalTemplate = `
+    <div class="recommendation-page">
+      <p>Para quem inspirou em uma ação ou projeto específico</p>
+      <label class="recommendation-page" for="model-recommendation" >Escreva sua mensagem aqui:</label>
+      <input class="recommendation-page" id="model-recommendation" required>
+      <button class="recommendation-page" id="submit-recommendation">Enviar</button>
+    </div>
+    `
+    recommendationForm.innerHTML += modalTemplate
+  })
+
+  referenceBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('clicou');
+    const modalTemplate = `
+    <div class="recommendation-page">
+      <p>Para quem se destaca em alguma atitude.</p>
+      
+      <h2>Atitudes Safra</h2>
+      <div class="recommendation-page">
+        <input type="checkbox" id="specialist" name="specialist" value="specialist">
+        <label for="specialist">Especialista</label>
+      </div>
+
+      <div class="recommendation-page">
+        <input type="checkbox" id="partnership" name="partnership" value="partnership">
+        <label for="partnership">Parceiro</label>
+      </div>
+
+      <div class="recommendation-page">
+        <input type="checkbox" id="resilience" name="resilience" value="resilience">
+        <label for="resilience">Resiliência</label>
+      </div>
+
+      <div class="recommendation-page">
+        <input type="checkbox" id="owner-attitude" name="owner-attitude" value="owner-attitude">
+        <label for="owner-attitude">Atitude de Dono</label>
+      </div>
+
+      <div class="recommendation-page">
+        <input type="checkbox" id="leadership" name="leadership" value="leadership">
+        <label for="leadership">Liderança</label>
+      </div>
+
+      <label class="recommendation-page" for="model-recommendation" >Escreva sua mensagem aqui:</label>
+      <input class="recommendation-page" id="model-recommendation" required>
+
+      <button class="recommendation-page" id="submit-recommendation">Enviar</button>
+    </div>
+    `
+    recommendationForm.innerHTML += modalTemplate
+  })
+
+  differenceBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('clicou');
+    const modalTemplate = `
+    <div class="recommendation-page">
+      <p>Para quem ajuda as pessoas e os times a se desenvolverem: com feedbacks, dicas, orientações.</p>
+      
+      <h2>Tópicos</h2>
+      <div class="recommendation-page">
+        <input type="checkbox" id="feedback" name="feedback" value="feedback">
+        <label for="feedback">Feedback</label>
+      </div>
+
+      <div class="recommendation-page">
+        <input type="checkbox" id="hints-tips" name="hints-tips" value="hints-tips">
+        <label for="hints-tips">Dicas e Sugestões</label>
+      </div>
+
+      <div class="recommendation-page">
+        <input type="checkbox" id="support" name="support" value="support">
+        <label for="support">Apoio em Desafios</label>
+      </div>
+
+      <div class="recommendation-page">
+        <input type="checkbox" id="owner-attitude" name="owner-attitude" value="owner-attitude">
+        <label for="owner-attitude">Atitude de Dono</label>
+      </div>
+
+      <label class="recommendation-page" for="model-recommendation" >Escreva sua mensagem aqui:</label>
+      <input class="recommendation-page" id="model-recommendation">
+
+      <button class="recommendation-page" id="submit-recommendation">Enviar</button>
+    </div>
+    `
+    recommendationForm.innerHTML += modalTemplate
+  })
+
+
   return rootElement;
 };
