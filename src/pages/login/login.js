d55cd325abc873c34246285be4428a0469d42740
gabriella-guimaraes <gabigara13@gmail.com>
2021-04-13 19:22:47 -0300

    DOM elements e redirecionamento para a register page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,22 +1,40 @@
+import { onNavigate } from '../../utils/history.js';
+
 export const Login = () => {
-// Coloque sua página
 const rootElement = document.createElement('div');
 rootElement.innerHTML = `
   <main class="container">
     <h2>Login</h2>
     <form action="">
       <div class="input-field">
-        <input type="text" name="username" id="username" placeholder="Insira seu email">
+        <input type="text" name="email" id="email" placeholder="Insira seu email">
         <div class="underline"></div>
       </div>
       <div class="input-field">
         <input type="password" name="password" id="password" placeholder="Insira sua senha">
           <div class="underline"></div>
       </div>
-        <input type="submit" value="Entrar">
+        <input type="submit" id="submit" value="Entrar">
     </form>
     <span>Esqueceu a senha?</span>
     <p>Não tem conta?<button id="btnCadastro">Cadastre-se</button></p>
 `;
+
+const email = rootElement.querySelector('#email');
+const password = rootElement.querySelector('#password');
+const registerBtn = rootElement.querySelector('#btnCadastro');
+const submit = rootElement.querySelector('#submit');
+
+submit.addEventListener('click', () => {
+  console.log(email.value);
+  console.log(password.value);
+
+})
+
+
+registerBtn.addEventListener('click', () => {
+  onNavigate('/register');
+})
+
 return rootElement;
 };
