3e5bcb0373b828a8d83fadda77e76f0f13cdaaaa
LuanaGss <luana_gss@hotmail.com>
2021-04-13 19:50:50 -0300

    atualizar

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -1,8 +1,17 @@
 export const Feed = () => {
-  // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
       <h1>Feed de recomendações</h1>
-  `;
+
+      <div class="filterSelect">
+        <select name="filter" id="filter">
+          <option selected>Filtro</option>
+          <option>Inspiração do projeto</option>
+          <option>Referência em atitudes Safra</option>        
+          <option>Diferença no dia a dia</option>
+          <option>Área de atuação</option> 
+        </select>     
+      </div> 
+  `;  
   return rootElement;
 };
