055aa063ca0b11a97557e938af1b16f6c967e10a
JessicaCavalcante <71466975+JessicaCavalcante@users.noreply.github.com>
2021-04-13 19:37:32 -0300

    Update feed.js

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -8,7 +8,8 @@ export const Feed = () => {
           <option selected>Filtro</option>
           <option>Inspiração do projeto</option>
           <option>Referência em atitudes Safra</option>        
-          <option>Diferença no dia a dia</option>              
+          <option>Diferença no dia a dia</option>
+          <option>Área de atuação</option> 
         </select>     
       </div> 
   `;  
