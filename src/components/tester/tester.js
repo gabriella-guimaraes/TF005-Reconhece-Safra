eb75fd12e3712fc25278960a0a430d2ec01691b6
sara <you@example.com>
2021-04-15 02:34:06 -0300

    CODE: Top card adicionado no feed

---
+++ src/components/tester/tester.js
@@ -0,0 +1,20 @@
+export const Tester = () => {
+  const rootElement = document.createElement('div');
+  rootElement.innerHTML = `
+    
+  <div class="jss6">
+  <div class="jss8"><img class="jss9" src="https://thispersondoesnotexist.com/image" style="height: 160px;
+  border-radius: 80px;
+" alt="logo">
+    <div class="nomesCargos">
+      <h1>Bem-Vindo(a): Sara Viana Carneiro</h1>
+        <h1>Área: Cargo</h1>
+        <h1 style="padding-top": 30px>Você : Cargo</h1>
+    </div>
+    
+  </div>
+</div>
+   
+    `;
+  return rootElement;
+}
